8d1c60eea2a0e6a8d58914f7d74d05fe0a0b8932
Emilio <emiliodavidr@gmail.com>
2017-12-29 10:32:35 -0800

    so it begins

---
+++ src/ft_printf.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eramirez <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/22 20:30:01 by eramirez          #+#    #+#             */
+/*   Updated: 2017/12/28 12:40:20 by eramirez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "includes/ft_printf.h"
+
+int ft_printf(const char *format, ...)
+{
+	int finished;
+	va_list args;
+
+	va_start(args, format);
+
+	va_end(args)
+	return(finished);
+}
